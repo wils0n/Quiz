42207447abe0e1e5151f4fc89e14c3dae37dc229
wils0n <wiljm0.0@gmail.com>
2016-03-14 15:46:21 -0500

    init componentes and conections

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbQuestion: UILabel!
+    @IBOutlet weak var imgQuestion: UIImageView!
+    @IBOutlet weak var btnAnswer1: UIButton!
+    @IBOutlet weak var btnAnswer2: UIButton!
+    @IBOutlet weak var btnAnswer3: UIButton!
+    
+    @IBOutlet weak var viewFeedback: UIView!
+    @IBOutlet weak var lbFeedback: UILabel!
+    @IBOutlet weak var btnFeedback: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func chooseAnswer1(sender: AnyObject) {
+    }
+    
 
+    @IBAction func chooseAnswer2(sender: AnyObject) {
+    }
+    
+    @IBAction func chooseAnswer3(sender: AnyObject) {
+    }
+    
+    @IBAction func btnFeedbackAction(sender: AnyObject) {
+    }
 }
 
