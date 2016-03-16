2a05f7e9dd0fd38cb9d6ec55586a760da3484f70
wils0n <wiljm0.0@gmail.com>
2016-03-16 11:59:08 -0500

    quiz ended

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -22,6 +22,10 @@ class ViewController: UIViewController {
     
     var questions: [Question]!
     
+    var currentQuestion = 0
+    var grade = 0.0
+    var quizEnded = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -36,9 +40,45 @@ class ViewController: UIViewController {
         let q1answer2 = Answer(answer: "4,5 m", isCorrect: false)
         let question1 = Question(question: "Cuántos metros en promedio tiene una girafa macho adulta?", strImageFileName: "girafa", answers: [q1answer0, q1answer1, q1answer2])
         
-        //Falta
+        let q2answer0 = Answer(answer: "2300 kg", isCorrect: true)
+        let q2answer1 = Answer(answer: "3300 kg", isCorrect: false)
+        let q2answer2 = Answer(answer: "4300 kg", isCorrect: false)
+        let question2 = Question(question: "Cuánto pesa en promedio un rinoceronte-branco macho adulto?", strImageFileName: "rinoceronte", answers: [q2answer0, q2answer1, q2answer2])
+
+        let q3answer0 = Answer(answer: "64 km/h", isCorrect: true)
+        let q3answer1 = Answer(answer: "74 km/h", isCorrect: false)
+        let q3answer2 = Answer(answer: "54 km/h", isCorrect: false)
+        let question3 = Question(question: "Cuál es la velocidad de una zebra?", strImageFileName: "zebra", answers: [q3answer0, q3answer1, q3answer2])
         
-        questions = [question0, question1]
+        questions = [question0, question1, question2, question3]
+        
+        startQuiz()
+    }
+    
+    func startQuiz(){
+        questions.shuffle()
+        for(var i=0; i < questions.count; i++){
+            questions[i].answers.shuffle()
+        }
+        quizEnded = false
+        grade = 0.0
+        currentQuestion = 0
+        
+        showQuestion(0) // muestra la primera pregunta
+    }
+    
+    func showQuestion(questionid: Int){
+
+        btnAnswer1.enabled = true
+        btnAnswer2.enabled = true
+        btnAnswer3.enabled = true
+        
+        lbQuestion.text = questions[questionid].strQuestion
+        imgQuestion.image = questions[questionid].imgQuestion
+        btnAnswer1.setTitle(questions[questionid].answers[0].strAnswer, forState: UIControlState.Normal)
+        btnAnswer2.setTitle(questions[questionid].answers[1].strAnswer, forState: UIControlState.Normal)
+        btnAnswer3.setTitle(questions[questionid].answers[2].strAnswer, forState: UIControlState.Normal)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,16 +88,68 @@ class ViewController: UIViewController {
 
     
     @IBAction func chooseAnswer1(sender: AnyObject) {
+        selectAnswer(0)
     }
     
 
     @IBAction func chooseAnswer2(sender: AnyObject) {
+        selectAnswer(1)
     }
     
     @IBAction func chooseAnswer3(sender: AnyObject) {
+        selectAnswer(2)
+    }
+    
+    func selectAnswer(answerid: Int){
+        btnAnswer1.enabled = false
+        btnAnswer2.enabled = false
+        btnAnswer3.enabled = false
+        
+        viewFeedback.hidden = false
+        
+        var answer: Answer = questions[currentQuestion].answers[answerid]
+        
+        if(answer.isCorrect == true){
+            grade = grade + 1.0
+            lbFeedback.text = answer.strAnswer + "\n\n Respuesta Correcta!"
+        }else{
+            lbFeedback.text = answer.strAnswer + "\n\n Respuesta Incorrecta..."
+        }
+        
+        if (currentQuestion < questions.count - 1){
+            btnFeedback.setTitle("Próxima", forState: UIControlState.Normal)
+        }else{
+            btnFeedback.setTitle("Ver Nota", forState: UIControlState.Normal)
+        }
+        
     }
     
     @IBAction func btnFeedbackAction(sender: AnyObject) {
+        viewFeedback.hidden = true
+        
+        if quizEnded{
+            startQuiz()
+        }else{
+            nextQuestion()
+        }
     }
+    
+    func nextQuestion(){
+        currentQuestion++
+        if(currentQuestion < questions.count){
+            showQuestion(currentQuestion)
+        }else{
+            endQuiz()
+        }
+    }
+    
+    func endQuiz(){
+        grade = grade/Double(questions.count)*100.0
+        quizEnded = true
+        viewFeedback.hidden = false
+        lbFeedback.text = "Su nota: \(grade)"
+        btnFeedback.setTitle("Reintentar", forState: UIControlState.Normal)
+    }
+    
 }
 
